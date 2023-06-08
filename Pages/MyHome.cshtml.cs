5cb68f37bb1b6c7b3c4b3e25cb9cb7f64659ee56
ThanhHa <119502362+NguyenThanhHaa@users.noreply.github.com>
2023-06-08 19:03:55 +0700

    Add files via upload

---
+++ Pages/MyHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthSystem.Areas.Identity.Pages
+{
+    public class MyHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Space.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthSystem.Areas.Identity.Pages
+{
+    public class SpaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
